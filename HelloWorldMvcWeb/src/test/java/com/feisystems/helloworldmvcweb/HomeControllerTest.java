048027f00625e620f6e9b67ddf2edbbf73952784
tlin-fei <tao.lin@feisystems.com>
2013-06-06 13:04:12 -0400

    Add one unit test to exercise constructor

---
+++ HelloWorldMvcWeb/src/test/java/com/feisystems/helloworldmvcweb/HomeControllerTest.java
@@ -0,0 +1,12 @@
+package com.feisystems.helloworldmvcweb;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+	
+	@Test
+	public void home(){
+		HomeController controller = new HomeController();
+	}
+
+}
